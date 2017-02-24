06b70073f283d7b7427bdd5336338795dea4a0a2
华文澜 <winlandiano@gmail.com>
2017-02-24 17:38:06 +0100

    implement progress effect on de

--- hitandrun_de.py
+++ hitandrun_de.py
@@ -1,10 +1,13 @@
 import os
 import os.path
+import sys
 
 hard_code_stuff_lenth = 1600
 
 
 def operate(in_filepath, out_filepath):
+    global finish_size
+    global total_size
     in_file = open(in_filepath, "rb")
     out_dir = os.path.dirname(out_filepath)
     if not os.path.exists(out_dir):
@@ -14,14 +17,24 @@ def operate(in_filepath, out_filepath):
     in_file.read(hard_code_stuff_lenth)
     while True:
         buff = in_file.read(1048576)
+        finish_size += len(buff)
+        sys.stdout.write("\r" + str(finish_size) + "/" + str(total_size) + " Bytes finished!")
+        sys.stdout.flush()
         out_file.write(buff)
         if len(buff) != 1048576:
             break
     in_file.close()
     out_file.close()
 
+def cal_total_size(file_path):
+    global total_size
+    total_size += os.path.getsize(filename)
+
 
 if __name__ == '__main__':
+    total_size = 0
+    finish_size = 0
+
     print('here is to REMOVE the stuff in a file or directory')
 
     inputpath = input("input file path=>")
@@ -32,6 +45,11 @@ if __name__ == '__main__':
 
     if os.path.isdir(inputpath):
         print("directory detected")
+        print("calculating directory size...")
+        for root, dirs, files in os.walk(inputpath):
+            for filename in files:
+                cal_total_size(os.path.joinroot, filename)
+
         for root, dirs, files in os.walk(inputpath):
             for filename in files:
                 in_filepath = os.path.join(root, filename)
@@ -39,7 +57,8 @@ if __name__ == '__main__':
                 operate(in_filepath, out_filepath)
     else:
         print("file detected")
+        total_size = os.path.getsize(inputpath)
         out_filepath = os.path.join(outputpath, os.path.basename(inputpath))
         operate(inputpath, out_filepath)
 
-    print("They've been back!")
+    print("\nThey've been back!")
